3b514074a6341810b84f39b1c3216f5092e3adc4
Luiz Henrique Souza <henryque.souz4@gmail.com>
2021-06-06 11:43:02 -0400

    create buttons for quiz

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,3 +1,4 @@
+import 'package:dev_quiz/challenge/widgets/next-button/next-button-widget.dart';
 import 'package:dev_quiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:dev_quiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +21,28 @@ class _ChallengePageState extends State<ChallengePage> {
       ),
       body: QuizWidget(
         title: "O que o Flutter faz em sua totalidade?",
+      ),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                  child: NextButtonWidget.white(
+                label: "Pular",
+                onTap: () {},
+              )),
+              SizedBox(width: 7),
+              Expanded(
+                  child: NextButtonWidget.green(
+                label: "Confirmar",
+                onTap: () {},
+              )),
+            ],
+          ),
+        ),
       ),
     );
   }
